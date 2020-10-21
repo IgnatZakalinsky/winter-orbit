e91e567f20d15526aafc70ff18f92d1e3743455d
IgnatZakalinsky <ai73a@yandex.ru>
2020-10-21 18:34:33 +0300

    Updates

---
+++ precache-manifest.459e9f35b04b0de8f5957557bf109b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fe6e0c4bda5db07f75503c920133e8",
+    "url": "/winter-orbit/index.html"
+  },
+  {
+    "revision": "13cc0a9f8b620ffdb01a",
+    "url": "/winter-orbit/static/css/main.84a41eea.chunk.css"
+  },
+  {
+    "revision": "faa74d41e01e3d0cd612",
+    "url": "/winter-orbit/static/js/2.c4c42299.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/winter-orbit/static/js/2.c4c42299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cc0a9f8b620ffdb01a",
+    "url": "/winter-orbit/static/js/main.4cd54c28.chunk.js"
+  },
+  {
+    "revision": "768e1010c4357b098ce1",
+    "url": "/winter-orbit/static/js/runtime-main.0717d058.js"
+  }
+]);
