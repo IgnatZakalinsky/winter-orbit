42c1308d81b58ed3d907e26b076933396bb14589
IgnatZakalinsky <ai73a@yandex.ru>
2020-11-13 14:14:59 +0300

    fix url2

--- src/i1-main/m3-dal/instance.ts
+++ src/i1-main/m3-dal/instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseURL = 'https://31.131.28.206:8008/'
+export const baseURL = 'http://31.131.28.206:8008/'
 // export const baseURL = 'http://127.0.0.1:8000/'
 
 export const instance = axios.create({
